57281780a4f56cd705669cac6e48e1a03d5e7bc8
w89477 <w89477>
2010-03-11 06:39:25 +0000

    default definition for the webapps

---
+++ src/javascript/autocomplete/device-invariant/autocomplete_defaultAdapter.js
@@ -0,0 +1,36 @@
+/***************************************** 
+ * YELLOW DEFAULT PARAMETERS
+ */
+_YM_ = {};
+
+_YM_.defaultURL = "http://dyela.yellowpages.sensis.com.au/suggest/business";
+
+_YM_.defaultPreAdaptor = function(query) {
+	return "query=" + query;
+}
+
+/** the default behaviour of the autocomplete is based on YM's json data structure
+ * hence YM doesn't need the default post adaptor
+ */
+_YM_.defaultPostAdaptor = null;
+
+/***************************************** 
+ * WHITE DEFAULT PARAMETERS
+ */
+_WPM_ = {};
+
+_WPM_.defaultPreAdaptor = function(query) {
+	return "q=" + query + "&limit=7&timestamp=1250816409097&type=businessName";
+}
+
+_WPM_.defaultURL = "http://www.whitepages.com.au/wp/autosuggest/autoSuggest.x";
+	
+_WPM_.defaultPreAdaptor = function(query) {
+	return "q=" + query + "&limit=7&timestamp=1250816409097&type=businessName";
+}
+
+_WPM_.defaultPostAdaptor = function(resultText) {
+	/* replace any occurance of \n (new line character) with , (comma) */
+	var afterReplacement = resultText.replace(new RegExp('\n', 'g'), ",");
+	return "{'suggestions': [" + afterReplacement + "]}";
+}
